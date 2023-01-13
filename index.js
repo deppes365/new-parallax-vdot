b8b224c9688d492aa950926776004c86080ebc46
Darren Eppes <deppes365@gmail.com>
2023-01-13 12:08:03 -0500

    more home edits made

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 'use strict';
 
-///// Constants ////
+///// Variables ////
+
+const banner = document.querySelector('.banner');
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-links');
+const aside = document.querySelector('aside');
+const heroContent = document.querySelector('.home-hero-content');
+
+let bannerHeight = banner.clientHeight;
+let asideHeight = aside.clientHeight;
+let heroContentHeight = heroContent.clientHeight;
 
 ///// Event Listeners /////
+
+window.addEventListener('resize', () => {
+	bannerHeight = banner.clientHeight;
+	heroContentHeight = heroContent.clientHeight;
+	asideHeight = aside.clientHeight;
+});
+
+// Function to make hero content fade out on scroll up
+window.addEventListener('scroll', () => {
+	const heroContentCenter =
+		heroContent.getBoundingClientRect().top +
+		(heroContent.getBoundingClientRect().bottom -
+			heroContent.getBoundingClientRect().top) /
+			2;
+
+	const opacityPercent = Math.floor((window.scrollY / heroContentCenter) * 100);
+
+	if (window.scrollY <= heroContentCenter) {
+		heroContent.style.opacity = `${100 - opacityPercent}%`;
+		console.log('changing');
+	}
+});
 
 // Handles mobile menu opening/closing
 hamburger.addEventListener('click', () => {
@@ -13,19 +43,19 @@ hamburger.addEventListener('click', () => {
 });
 
 menu.addEventListener('click', () => {
-    if(hamburger.classList.contains('active')) {
-        menu.classList.add('inactive')
-        hamburger.classList.remove('active')
-    }
+	if (hamburger.classList.contains('active')) {
+		menu.classList.add('inactive');
+		hamburger.classList.remove('active');
+	}
 });
-
 
 ///// Intersection Observers /////
 
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        
-    })
-}, {
-    threshold: 0.1
-})
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach(entry => {});
+	},
+	{
+		threshold: 0.1,
+	}
+);
