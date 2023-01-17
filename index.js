3bae105771312c497e8240fba7a3e1f298a67f27
Darren Eppes <deppes365@gmail.com>
2023-01-17 11:20:47 -0500

    menu open scroll fix

--- index.js
+++ index.js
@@ -41,11 +41,11 @@ hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	menu.classList.toggle('inactive');
 
-	// if(hamburger.classList.contains('active')) {
-	// 	document.querySelector('html').style.overflow = 'hidden';
-	// } else {
-	// 	document.querySelector('html').style.overflow = 'auto';
-	// }
+	if(hamburger.classList.contains('active')) {
+		document.querySelector('html').style.overflow = 'hidden';
+	} else {
+		document.querySelector('html').style.overflow = 'auto';
+	}
 });
 
 menu.addEventListener('click', () => {
